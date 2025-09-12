9bb5b63ae2cbae02264d53909a08f07a1fe48ff1
Dennis <74433222+HovKlan-DH@users.noreply.github.com>
2025-09-12 14:49:38 +0200

    Update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2025.09.12.3")]
-[assembly: AssemblyFileVersion("2025.09.12.3")]
+[assembly: AssemblyVersion("2025.09.12.4")]
+[assembly: AssemblyFileVersion("2025.09.12.4")]
